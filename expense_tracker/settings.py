c86182e724eae15a68a468cd5c63d3b8cc6e242a
Git-Github <kumarbablu03240@gmail.com>
2025-09-15 18:01:48 +0530

    latest

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 # Allowed hosts from environment
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost,expense-tracker-7dau.onrender.com").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
